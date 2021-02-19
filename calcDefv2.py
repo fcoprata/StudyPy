9e19ceeab935ef2a550d7e1abb24977d228e89f8
fcoprata <fcoprata@alu.ufc.br>
2021-02-19 16:07:40 -0300

    Calculadora usando Método,parte2

---
+++ calcDefv2.py
@@ -0,0 +1,22 @@
+n1 = int(input('Primeiro Número : '))
+n2 = int(input('Segundo Número : '))
+
+class Calculadora:
+    # Opcional já que não inicializa
+    # def __init__(self):
+    #     pass
+
+    def soma(self,a,b):
+        return a + b
+    def sub(self,a,b):
+        return a - b
+    def mult(self,a,b):
+        return a * b
+    def div(self,a,b):
+        return a / b
+
+calculadora = Calculadora()
+print(calculadora.soma(n1,n2))
+print(calculadora.mult(n1,n2))
+print(calculadora.div(n1,n2))
+print(calculadora.sub(n1,n2))
