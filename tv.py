6bb3c7de8ce47b1495317cfa51e38a09893abdcd
fcoprata <fcoprata@alu.ufc.br>
2021-02-21 00:59:03 -0300

    Atualização no código

--- tv.py
+++ tv.py
@@ -12,17 +12,18 @@ class Tv:
     def leschanel(self):
         self.canal -= 1
 televisão = Tv()
-power = input('Deseja ligar Tv?')
-if power.upper()[0] == 'S':
-    if televisão.ligada == False:
-        televisão.power()
-        print('Tv ligando')
-        chanel = input('Deseja mudar de canal?')
-    if chanel.upper()[0] == 'S':
-        change = input('Mudar para mais ou menos? ')
-        if change.lower() == 'mais':
-            televisão.plschanel()
-            print(f'A canal agora é {televisão.canal}')
-        elif change.lower() == 'menos':
-            televisão.leschanel()
-            print(f'O canal agora é {televisão.canal}')
+if __name__ == '__main__':
+    power = input('Deseja ligar Tv?')
+    if power.upper()[0] == 'S':
+        if televisão.ligada == False:
+            televisão.power()
+            print('Tv ligando')
+            chanel = input('Deseja mudar de canal?')
+        if chanel.upper()[0] == 'S':
+            change = input('Mudar para mais ou menos? ')
+            if change.lower() == 'mais':
+                televisão.plschanel()
+                print(f'A canal agora é {televisão.canal}')
+            elif change.lower() == 'menos':
+                televisão.leschanel()
+                print(f'O canal agora é {televisão.canal}')
