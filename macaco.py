a94b5df2b74a89f47ab5ca3174c668f1f9c131c5
fcoprata <fcoprata@alu.ufc.br>
2021-05-29 00:17:41 -0300

    macaco

---
+++ macaco.py
@@ -0,0 +1,12 @@
+print("TESTE DO MAMACO")
+print("A-""\U0001F648","B-""\U0001F649","C-""\U0001F64A")
+n = input("Qual macaco te define hoje? ")
+A ="\U0001F648"
+B ="\U0001F649"
+C ="\U0001F64A"
+if n.upper() == 'A':
+    print(35*"\U0001F648")
+elif n.upper() == 'B':
+    print(35*"\U0001F649")
+elif n.upper() == 'C':
+    print(35*"\U0001F64A")
